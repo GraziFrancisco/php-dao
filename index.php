e02199ad6f6c1ed91155893f76d4fc204e8d6cd9
Grazieli <grazilachica@outlook.com>
2023-07-27 09:08:58 -0300

    metodos de lista

--- index.php
+++ index.php
@@ -3,7 +3,29 @@
 
 require_once("config.php");
 
-$user = new Usuario();
-$user->loadById(10);
 
-echo $user;
+// CARREGA UM USUARIO
+
+//$user = new Usuario();
+//$user->loadById(10);
+//echo $user;
+
+
+// CARREGA UMA LISTA DE USUARIOS
+
+//$lista = Usuario::getList();
+
+//echo json_encode($lista);
+
+// CARREGA UM USUARIO ESPECIFICO
+
+//$search = Usuario::search("a");
+
+//echo json_encode($search);
+
+// CARREGA UM USUARIO USANDO LOGIN E SENHA
+
+$usuario = new Usuario();
+$usuario->login("Joana", "12345");
+
+echo $usuario;
